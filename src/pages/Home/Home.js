f0b8708a98e54c9a934c612e0df24f200efd7468
mateotiedra <mateo.tiedra@outlook.com>
2024-09-13 22:40:01 +0200

    add footer + basic components

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+
+import NoisyContainer from '../../components/NoisyContainer/NoisyContainer';
+import Footer from '../../components/Footer';
+
+
+function Home() {
+
+  return (
+    <>
+      <h1>Artiphys</h1>
+      <h4>Artiphys</h4>
+      <p>Artiphys</p>
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
