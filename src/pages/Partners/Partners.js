180fdffe8d36062fd0fd9881d4b1748034f62b79
mateotiedra <mateo.tiedra@outlook.com>
2024-09-20 12:23:07 +0200

    bettre style of partners

--- src/pages/Partners/Partners.js
+++ src/pages/Partners/Partners.js
@@ -10,21 +10,29 @@ function Partners() {
     <>
       <Navbar />
       <div className='section-container'>
-        <h2 className='pb-h2 text-center'>Nos Partenaires</h2>
-        <div className='inline-flex flex-wrap items-stretch gap-3 justify-around'>
+        <h2 className='text-h3 sm:text-h2 pb-h2 text-center'>
+          Nos Partenaires
+        </h2>
+        <p className='text-center pb-8'>
+          Un immense merci à nos partenaires pour leur précieux soutien. Votre
+          contribution est essentielle à l'association Artiphys, permettant de
+          créer des événements inoubliables et de promouvoir la culture au sein
+          de l'EPFL.
+        </p>
+        <div className='inline-flex flex-wrap items-stretch gap-5 md:gap-9 justify-center'>
           {PARTNERS_LINKS.map((partner) => (
             <a
               href={partner.url}
-              className='px-4 flex items-center rounded-lg h-32 bg-base'
+              className='px-4 flex items-center rounded-lg h-16 bg-base'
               style={{ backgroundColor: partner.color || '' }}
               key={partner.url}
             >
-              <img src={partner.src} alt='partner' width={200} />
+              <img src={partner.src} alt='partner' width={100} />
             </a>
           ))}
         </div>
       </div>
-      <Footer />
+      <Footer light />
     </>
   );
 }
